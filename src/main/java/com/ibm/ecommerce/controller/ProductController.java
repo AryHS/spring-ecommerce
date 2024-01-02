c4e5f7b0195a17fe78b9a07b740d9d8e807257bf
AryToño <aryantoniohs@gmail.com>
2024-01-01 21:34:39 -0600

    Se creó la función de guardar producto en la base de datos, vista create y logger para la clase ProductController

--- src/main/java/com/ibm/ecommerce/controller/ProductController.java
+++ src/main/java/com/ibm/ecommerce/controller/ProductController.java
@@ -1,14 +1,40 @@
 package com.ibm.ecommerce.controller;
 
+import com.ibm.ecommerce.model.Product;
+import com.ibm.ecommerce.model.User;
+import com.ibm.ecommerce.service.ProductService;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
+  //Variable auxiliar para manejar los Logs en las pruebas
+  private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+  @Autowired
+  private ProductService productService;
+
+  // Método para cargar la vista donde se muestran los productos
   @GetMapping("")
   public String show(){
     return "products/show";
+  }
+
+  //Método para cargar la vista donde se crea un producto
+  @GetMapping("/create")
+  public String create(){
+    return "products/create";
+  }
+  @PostMapping("/save")
+  public String save(Product product) {
+    LOGGER.info("Este es el objeto producto {}", product);
+    User user = new User(1, "", "", "", "", "");
+    product.setUser(user);
+    productService.save(product);
+    return "redirect:/products";
   }
 }
