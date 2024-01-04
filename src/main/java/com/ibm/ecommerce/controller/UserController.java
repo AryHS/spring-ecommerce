7e3df125989c6e8b167ba8266e7c18c13063d757
AryToño <aryantoniohs@gmail.com>
2024-01-04 16:54:42 -0600

    Se creó la lógica para mostrar la vista purchases para el usuario

--- src/main/java/com/ibm/ecommerce/controller/UserController.java
+++ src/main/java/com/ibm/ecommerce/controller/UserController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -59,5 +60,12 @@ public class UserController {
     }
 
     return "redirect:/";
+  }
+
+  @GetMapping("/purchases")
+  public String getPurchases(Model model, HttpSession session){
+    model.addAttribute("session", session.getAttribute("idUser"));
+
+    return "user/purchases";
   }
 }
