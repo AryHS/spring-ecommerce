d4e632910d5bbd88ec44d4500dab99a28d7ad077
AryToño <aryantoniohs@gmail.com>
2024-01-04 23:00:13 -0600

    Se implementó la funcionalidad para ver los detalles de las ordenes para el ADMIN

--- src/main/java/com/ibm/ecommerce/controller/AdminController.java
+++ src/main/java/com/ibm/ecommerce/controller/AdminController.java
@@ -1,19 +1,25 @@
 package com.ibm.ecommerce.controller;
 
+import com.ibm.ecommerce.model.Order;
 import com.ibm.ecommerce.model.Product;
 import com.ibm.ecommerce.service.order.IOrderService;
 import com.ibm.ecommerce.service.product.ProductService;
 import com.ibm.ecommerce.service.user.IUserService;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+  private Logger LOGGER = LoggerFactory.getLogger(AdminController.class);
+
   @Autowired
   private ProductService productService;
   @Autowired
@@ -39,5 +45,14 @@ public class AdminController {
   public String orders(Model model){
     model.addAttribute("ordersList", orderService.findAll());
     return "admin/orders";
+  }
+  @GetMapping("/details/{id}")
+  public String details(Model model, @PathVariable Integer id){
+    LOGGER.info("Id de la orden de compra: {}", id);
+    Order order = orderService.findById(id).get();
+
+    model.addAttribute("details", order.getSummary());
+
+    return "admin/summary";
   }
 }
